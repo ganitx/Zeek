b3eeedc9955af33640df866ff834821e4051e729
Gani <agani2032@gmail.com>
2023-09-08 02:21:35 +0300

    added JSON data for initial setup

---
+++ Items.d.ts
@@ -0,0 +1,15 @@
+
+type Weaponse = {
+    id?: number;
+    Type: string;
+    Name: string;
+    SubType: string;
+    Weapon: (number | string);
+    Accuracy: (number | string);
+    Damage: (number | string);
+    Range: (number | string);
+    FireRate: (number | string);
+    Mobility: (number | string);
+    Control: (number | string);	
+    Pros: string;
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { attachments } from '@/lib/getItemsInfo';
 
-export default function Component() {
-    return (
-        <div>Gani</div>
+export default function MainPage() {
+
+    const switcher = (num: number | undefined) => {
+        if (!(num === undefined) && num === 0) {
+            return "bg-red-700"; 
+        }
+    };
+
+    const WeaponseContent: React.JSX.Element = (
+        <section>
+            <div 
+                className='bg-zinc-900 flex justify-between
+                w-8/12 h-auto p-4'
+            >
+                {attachments.map((items: Weaponse) => (
+                    <div 
+                        key={items.id}
+                        className={`${switcher(items.id)} w-1/2 p-3
+                        leading-9`}
+                    >
+                        <div>{items.Name}</div>
+                    </div>
+                ))}
+            </div>
+        </section>
     );
+
+    return WeaponseContent;
 }
